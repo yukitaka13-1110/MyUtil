98a56e6d1e7e42042a13173d4141cad25a111da1
yukitaka13-1110 <yukitaka13.1110@gmail.com>
2018-08-23 15:50:42 +0900

    For a plurality of processing

--- my_util.py
+++ my_util.py
@@ -4,12 +4,28 @@ import time
 class Measure:
 
     def __init__(self):
-        self._start = 0
-        self._stop = 0
+        self._start = {}
+        self._stop = {}
 
-    def start(self):
-        self._start = time.time()
+    def start(self, process):
+        self._start[process] = time.time()
 
     def stop(self, process):
-        self._stop = time.time()
-        print(process+" End : ",str(self._stop-self._start))
+        if process not in self._start:
+            print("Measure: No such process '%s'"%process)
+            return
+        self._stop[process] = time.time()
+
+    def time(self, process):
+        if process not in self._start or process not in self._stop:
+            print("Measure: No such process '%s'"%process)
+            return
+        print("Time of %s ==> "%process,self._stop[process]-self._start[process])
+
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    main()
